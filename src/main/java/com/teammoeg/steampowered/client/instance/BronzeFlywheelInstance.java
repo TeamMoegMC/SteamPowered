fd729f2f5fe139ee3254a9fa1cdd2435582fcf6a
yuesha-yc <kevinwangyichen@gmail.com>
2021-10-24 16:55:17 -0700

    Update licenses

--- src/main/java/com/teammoeg/steampowered/client/instance/BronzeFlywheelInstance.java
+++ src/main/java/com/teammoeg/steampowered/client/instance/BronzeFlywheelInstance.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021 TeamMoeg
+ *
+ * This file is part of Steam Powered.
+ *
+ * Steam Powered is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * Steam Powered is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Steam Powered. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.teammoeg.steampowered.client.instance;
 
 import com.google.common.collect.Lists;
--- src/main/java/com/teammoeg/steampowered/client/render/AlternatorRenderer.java
+++ src/main/java/com/teammoeg/steampowered/client/render/AlternatorRenderer.java
@@ -1,3 +1,30 @@
+/**
+ * Available under MIT the license more info at: https://tldrlegal.com/license/mit-license
+ *
+ * MIT License
+ *
+ * Copyright 2021 MRH0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction,
+ * including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom t
+ * he Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.teammoeg.steampowered.client.render;
 
 import com.simibubi.create.AllBlockPartials;
@@ -7,6 +34,10 @@ import com.simibubi.create.foundation.render.PartialBufferer;
 import com.simibubi.create.foundation.render.SuperByteBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 
+/**
+ * Adapted from: Create: Crafts & Additions
+ * @author MRH0
+ */
 public class AlternatorRenderer extends KineticTileEntityRenderer {
 
     public AlternatorRenderer(TileEntityRendererDispatcher dispatcher) {
