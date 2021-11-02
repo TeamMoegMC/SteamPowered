b238789fd5cca918374c19626249e0fecdb03f58
khj xiaogu <54445543+khjxiaogu@users.noreply.github.com>
2021-11-02 23:26:34 +0800

    remove steam block

--- src/main/java/com/teammoeg/steampowered/BlockRegistry.java
+++ src/main/java/com/teammoeg/steampowered/BlockRegistry.java
@@ -27,5 +27,5 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, SteamPowered.MODID);
-    public static RegistryObject<FlowingFluidBlock> steamBlock = BLOCKS.register("steam", () -> new FlowingFluidBlock(FluidRegistry.steam, Block.Properties.of(Material.WATER).noCollission().strength(100.0F).noDrops()));
+    //public static RegistryObject<FlowingFluidBlock> steamBlock = BLOCKS.register("steam", () -> new FlowingFluidBlock(FluidRegistry.steam, Block.Properties.of(Material.WATER).noCollission().strength(100.0F).noDrops()));
 }
