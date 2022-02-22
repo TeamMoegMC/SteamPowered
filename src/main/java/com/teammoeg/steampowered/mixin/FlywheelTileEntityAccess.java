eecc7849a82b6c86e203274af6ab91676b7a86fb
yuesha-yc <kevinwangyichen@gmail.com>
2022-02-21 20:41:48 -0800

    Update forge and use Parchment Mappings

--- src/main/java/com/teammoeg/steampowered/mixin/FlywheelTileEntityAccess.java
+++ src/main/java/com/teammoeg/steampowered/mixin/FlywheelTileEntityAccess.java
@@ -18,13 +18,11 @@
 
 package com.teammoeg.steampowered.mixin;
 
+import com.simibubi.create.foundation.utility.animation.InterpolatedChasingValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import com.simibubi.create.content.contraptions.components.flywheel.FlywheelTileEntity;
-import com.simibubi.create.foundation.gui.widgets.InterpolatedChasingValue;
-
-import InterpolatedChasingValue;
 
 @Mixin(FlywheelTileEntity.class)
 public interface FlywheelTileEntityAccess {
