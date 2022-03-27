7365a7a19ea25e1ab18bb838bd007a86884fe3c9
yuesha-yc <kevinwangyichen@gmail.com>
2022-03-26 20:26:15 -0700

    Update to Create 0.4f amd Flywheel 0.6.1 Closes #30 #32

--- src/main/java/com/teammoeg/steampowered/client/instance/SteelFlywheelInstance.java
+++ src/main/java/com/teammoeg/steampowered/client/instance/SteelFlywheelInstance.java
@@ -25,7 +25,6 @@ import com.jozufozu.flywheel.api.Material;
 import com.jozufozu.flywheel.api.MaterialManager;
 import com.jozufozu.flywheel.api.instance.DynamicInstance;
 import com.jozufozu.flywheel.core.materials.model.ModelData;
-import com.jozufozu.flywheel.util.transform.MatrixTransformStack;
 import com.jozufozu.flywheel.util.transform.TransformStack;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.AllBlockPartials;
