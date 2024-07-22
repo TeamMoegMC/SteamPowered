4a01f4b9780bd720b1a82291d6cc59317b632a91
yuesha-yc <kevinwangyichen@gmail.com>
2024-07-21 20:16:53 -0400

    Port changes from 1.16.5:   Fix glitch in flywheel (commented out for now)

--- src/main/java/com/teammoeg/steampowered/mixin/MixinFlywheel.java
+++ src/main/java/com/teammoeg/steampowered/mixin/MixinFlywheel.java
@@ -1,50 +1,112 @@
-package com.teammoeg.steampowered.mixin;
-
-import com.simibubi.create.content.kinetics.base.GeneratingKineticBlockEntity;
-import com.teammoeg.steampowered.oldcreatestuff.OldEngineBlock;
-import com.teammoeg.steampowered.oldcreatestuff.OldEngineBlockEntity;
-import com.teammoeg.steampowered.oldcreatestuff.OldFlywheelBlock;
-import com.teammoeg.steampowered.oldcreatestuff.OldFlywheelBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import com.teammoeg.steampowered.content.engine.SteamEngineTileEntity;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.core.Direction;
-import net.minecraft.core.BlockPos;
-
-//@Mixin(OldFlywheelBlockEntity.class)
-//public abstract class MixinFlywheel extends GeneratingKineticBlockEntity {
-//	public MixinFlywheel(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-//		super(type, pos, state);
+//package com.teammoeg.steampowered.mixin;
+//
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Shadow;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//
+//import com.simibubi.create.content.contraptions.KineticNetwork;
+//import com.simibubi.create.content.contraptions.base.GeneratingKineticTileEntity;
+//import com.simibubi.create.content.contraptions.components.flywheel.FlywheelBlock;
+//import com.simibubi.create.content.contraptions.components.flywheel.FlywheelTileEntity;
+//import com.simibubi.create.content.contraptions.components.flywheel.engine.EngineBlock;
+//import com.simibubi.create.content.contraptions.components.flywheel.engine.EngineTileEntity;
+//import com.teammoeg.steampowered.content.engine.SteamEngineTileEntity;
+//
+//import net.minecraft.block.Block;
+//import net.minecraft.tileentity.TileEntity;
+//import net.minecraft.tileentity.TileEntityType;
+//import net.minecraft.util.Direction;
+//import net.minecraft.util.math.BlockPos;
+//
+//@Mixin(FlywheelTileEntity.class)
+//public abstract class MixinFlywheel extends GeneratingKineticTileEntity{
+//	public MixinFlywheel(TileEntityType<?> typeIn) {
+//		super(typeIn);
+//	}
+//	int errortick=0;
+//	void seemAsError() {
+//		if(this.getGeneratedSpeed()!=0)
+//			errortick++;
+//		if(errortick>=10) {
+//			errortick=0;
+//			this.setRotation(0,0);
+//		}
 //	}
 //	@Shadow(remap=false)
 //	public abstract void setRotation(float speed, float capacity);
-//	@Inject(at=@At("HEAD"),method="tick",remap = false)
+//	@Inject(at=@At("HEAD"),method="tick")
 //	public void sp$tick(CallbackInfo cbi) {
-//		Direction at=OldFlywheelBlock.getConnection(getBlockState());
+//		Direction at=FlywheelBlock.getConnection(getBlockState());
 //		if(at!=null) {
 //			BlockPos eng=this.getBlockPos().relative(at,2);
-//			Block b=this.getLevel().getBlockState(eng).getBlock();
-//			if(!(b instanceof OldEngineBlock)) {
-//				OldFlywheelBlock.setConnection(getLevel(),getBlockPos(),getBlockState(),null);
-//				this.setRotation(0,0);
+//			Block b=this.getWorld().getBlockState(eng).getBlock();
+//			if(!(b instanceof EngineBlock)) {
+//				FlywheelBlock.setConnection(getWorld(),getBlockPos(),getBlockState(),null);
+//				seemAsError();
 //			}else {
-//				BlockEntity te=this.getLevel().getBlockEntity(eng);
-//				if(te instanceof OldEngineBlockEntity) {
+//				TileEntity te=this.getWorld().getBlockEntity(eng);
+//				if(te instanceof EngineTileEntity) {
 //					if(te instanceof SteamEngineTileEntity) {
 //						SteamEngineTileEntity ete=(SteamEngineTileEntity) te;
 //						if(ete.getFlywheel()!=this.getBlockState().getBlock())
-//							this.setRotation(0,0);
+//							seemAsError();
 //					}
-//				}else this.setRotation(0,0);
+//				}else seemAsError();
 //			}
-//		}else this.setRotation(0,0);
+//		}else seemAsError();
+//	}
+//	/*@Override
+//	public void applyNewSpeed(float prevSpeed, float speed) {
+//
+//		// Speed changed to 0
+//		if (speed == 0) {
+//			if (hasSource()) {
+//				notifyStressCapacityChange(0);
+//				getOrCreateNetwork().updateStressFor(this, calculateStressApplied());
+//				return;
+//			}
+//			detachKinetics();
+//			setSpeed(0);
+//			setNetwork(null);
+//			return;
+//		}
+//
+//		// Now turning - create a new Network
+//		if (prevSpeed == 0) {
+//			setSpeed(speed);
+//			setNetwork(createNetworkId());
+//			attachKinetics();
+//			return;
+//		}
+//
+//		// Change speed when overpowered by other generator
+//		if (hasSource()) {
+//
+//			// Staying below Overpowered speed
+//			if (Math.abs(prevSpeed) >= Math.abs(speed)) {
+//				if (Math.signum(prevSpeed) != Math.signum(speed))
+//					level.destroyBlock(worldPosition, true);
+//				return;
+//			}
+//
+//			// Faster than attached network -> become the new source
+//			detachKinetics();
+//			setSpeed(speed);
+//			source = null;
+//			setNetwork(createNetworkId());
+//			attachKinetics();
+//			return;
+//		}
+//
+//		// Reapply source
+//		detachKinetics();
+//		setSpeed(speed);
+//		attachKinetics();
+//	}*/
+//	@Override
+//	public int getFlickerScore(){
+//		return 0;
 //	}
 //}
-//
