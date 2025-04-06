4d5983f85caf9fdcaaf8874b2c053babe98885e4
khjxiaogu <54445543+khjxiaogu@users.noreply.github.com>
2025-04-07 02:15:49 +0800

    flywheel would no longer break instead

--- src/main/java/com/teammoeg/steampowered/content/flywheel/SteamFlywheelTileEntity.java
+++ src/main/java/com/teammoeg/steampowered/content/flywheel/SteamFlywheelTileEntity.java
@@ -30,6 +30,58 @@ public class SteamFlywheelTileEntity extends OldFlywheelBlockEntity {
 
         clearState();
     }
+	public int getFlickerScore() {
+		return 0;
+	}
+	public void applyNewSpeed(float prevSpeed, float speed) {
+
+		// Speed changed to 0
+		if (speed == 0) {
+			if (hasSource()) {
+				notifyStressCapacityChange(0);
+				getOrCreateNetwork().updateStressFor(this, calculateStressApplied());
+				return;
+			}
+			detachKinetics();
+			setSpeed(0);
+			setNetwork(null);
+			return;
+		}
+
+		// Now turning - create a new Network
+		if (prevSpeed == 0) {
+			setSpeed(speed);
+			setNetwork(createNetworkId());
+			attachKinetics();
+			return;
+		}
+
+		// Change speed when overpowered by other generator
+		if (hasSource()) {
+
+			// Staying below Overpowered speed
+			if (Math.abs(prevSpeed) >= Math.abs(speed)) {
+				if (Math.signum(prevSpeed) != Math.signum(speed)&&speed!=0&&prevSpeed!=0){
+					level.destroyBlock(worldPosition, true);
+					System.out.println("break because overpowered");
+				}
+				return;
+			}
+
+			// Faster than attached network -> become the new source
+			detachKinetics();
+			setSpeed(speed);
+			source = null;
+			setNetwork(createNetworkId());
+			attachKinetics();
+			return;
+		}
+
+		// Reapply source
+		detachKinetics();
+		setSpeed(speed);
+		attachKinetics();
+	}
 
     public void clearState() {
         setRotation(0.0F, 0.0F);
